bc7b8c5ffd38fae41c85881221f5f6c8f02355a5
Thiago Brito <thiago.britos@berghem.com.br>
2020-06-04 16:14:54 -0300

    update account & statement

--- app/models/account.py
+++ app/models/account.py
@@ -1,21 +1,26 @@
 from app.models.agency import AgencyModel
+from app.models.statement import StatementModelList
+
 
 class AccountModel:
     def __init__(self):
         self._balance = 0
         self.agency = AgencyModel()
+        self._statements = StatementModelList()
     
     @property
     def balance(self):
         return self._balance
     
-    def deposit(self, value: int):
+    def deposit(self, value: int, description: str = None):
         self._balance += value
+        self._statements.add(balance=self.balance, description=description, deposit=value)
     
-    def withdraw(self, value: int):
+    def withdraw(self, value: int, description: str = None):
         self._balance -= value
+        self._statements.add(balance=self.balance, description=description, withdraw=value)
         return value
 
-    def transfer(self, value: int, account: 'AccountModel'):
-        account.deposit(value)
-        self.withdraw(value)
+    def transfer(self, value: int, account: 'AccountModel', description: str = None):
+        account.deposit(value, description)
+        self.withdraw(value, description)
--- app/models/statement.py
+++ app/models/statement.py
@@ -7,10 +7,26 @@ from pydantic import BaseModel
 class StatementModel(BaseModel):
     timestamp: datetime = datetime.now()
     description: str = None
-    withdrawal: int
-    deposit: int
+    withdraw: int = None
+    deposit: int = None
     balance: int
 
 
 class StatementModelList(BaseModel):
     statements: List[StatementModel]
+
+    def add(
+        self,
+        balance: int, 
+        description: str = None, 
+        withdraw: int = None, 
+        deposit: int = None
+        ):
+        self.statements.append(
+            StatementModel(
+                description=description,
+                withdraw=withdraw,
+                deposit=deposit,
+                balance=balance
+            )
+        )        
