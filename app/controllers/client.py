bbd1b99249b528fa10ed7c37e85a1ed07640430c
Thiago Brito <thiago.britos@berghem.com.br>
2020-06-03 21:15:18 -0300

    update client controller

--- app/controllers/client.py
+++ app/controllers/client.py
@@ -1,16 +1,18 @@
+from typing import Dict
+
 from app.models.client import ClientModel, ClientModelList
 from app.views.client import ClientView
 
+
 class ClientController:
     def __init__(self):
-        # self.client_model = ClientModel(name=name, cpf=cpf)
         self.client_view =  ClientView()
         self.clients = ClientModelList()
 
-    def add_client(self, data: dict):
-        self.clients.append(
+    def add_client(self, data: Dict[str, str]):
+        self.clients.push(
             ClientModel(**data)
-        )
-    
+
+
 
     
